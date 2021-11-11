b945c29db9c9ea6542a77be611573122dbd4dfd0
Сергей Кирюткин <kiryutkin88@yandex.ru>
2021-11-11 21:35:27 +0300

    first oop

---
+++ oop.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+@dataclass
+class Transport:
+    title: str
+    price: int
+    engine: Engine
+
+class Engine:
+    def __init__(self, power, title):
+        self.power = power
+        self.title = title
+        
+class Car:
+    def __init__(self, title, color, price, engine: Engine):
+        self.title = title
+        self.color = color
+        self.price = price
+        self.engine = engine
+
+    def show_price(self):
+        print(f'Это машина марки {self.title}, она стоит {self.price}')
+        print(f'Машина оборудована двигателем {self.engine.title} с мощностью {self.engine.power} л. с.')
+
+class Track:
+    def __init__(self, title, price, engine: Engine, bucket_volume):
+        self.title = title        
+        self.price = price
+        self.engine = engine
+        self.bucket_volume = bucket_volume
+
+class Bike(Transport):
+    def __init__(self, title, price, engine: Engine, sound):
+        self.title = title        
+        self.price = price
+        self.engine = engine
+        self.sound = sound
+
+class Buyer:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def car_buying(self, car: Car):
+        if self.balance < car.price:
+            insufficient_funds = car.price - self.balance
+            print(f'{self.name}, not enough money: {insufficient_funds}')
+        else:
+            self.balance -= car.price
+            print(f'{self.name}, congrats! This car is yours!')
+
+if __name__ == '__main__':
+    engine_for_volvo60 = Engine(200, 'D420')
+    engine_for_volvo90 = Engine(270, 'D520')
+    first_car = Car('Volvo XC60', 'Белый', 6500000, engine_for_volvo60)
+    second_car = Car('Volvo XC90', 'Черный', 7500000, engine_for_volvo90)
+    first_buyer = Buyer('Георгий', 1500000)
+    second_buyer = Buyer('Василий', 10000000)
+    #first_car.show_price()
+    #second_car.show_price()
+    print(second_buyer.balance)
+    second_buyer.car_buying(first_car)
+    print(second_buyer.balance)
+        
